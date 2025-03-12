4c31808cbd2e6c9303ad0652647f9f527e598282
bongti1 <cse2022200003@ppiu.edu.kh>
2025-03-12 18:28:00 +0700

    Fixed Suspence

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,62 +1,11 @@
 'use client';
-import { From } from "@/components/From";
-import { Suspense, useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense} from "react";
+import EditPrompt from "@/components/update";
 
 const UpdatePrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const paramsId = searchParams.get("id");
-  const [post, setPost] = useState({prompt:"Hello Son!", tag:""});
-  const [submitting, setSubmitting] = useState(false);
-
-  useEffect(() => {
-    const getPrompt = async () => {
-      const responce = await fetch(`/api/prompt/${paramsId}`);
-      const data = await responce.json();
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag
-      });
-    }
-    if(paramsId){
-      getPrompt();
-    }
-
-  },[paramsId])
-
-  const updatePrompt = async(e) => {
-    e.preventDefault
-    setSubmitting(true)
-
-    try {
-      const responce = await fetch(`/api/prompt/${paramsId}`, {
-        method: "PATCH",
-        body:JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag
-        })
-      });
-
-      if(responce.ok){
-        router.push("/");
-      }
-    } catch (error) {
-      console.log(error);
-    }finally{
-      setSubmitting(false);
-    }
-  }
   return (
     <Suspense>
-       <From 
-        type="Update "
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={<Suspense>{updatePrompt}</Suspense>}
-        >
-      </From>
+      <EditPrompt></EditPrompt>
     </Suspense>
   )
 }
---
+++ components/update.jsx
@@ -0,0 +1,64 @@
+'use client';
+import { Suspense, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { From } from "./From";
+
+const EditPrompt = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const paramsId = searchParams.get("id");
+    const [post, setPost] = useState({prompt:"Hello Son!", tag:""});
+    const [submitting, setSubmitting] = useState(false);
+  
+    useEffect(() => {
+      const getPrompt = async () => {
+        const responce = await fetch(`/api/prompt/${paramsId}`);
+        const data = await responce.json();
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag
+        });
+      }
+      if(paramsId){
+        getPrompt();
+      }
+  
+    },[paramsId])
+  
+    const updatePrompt = async(e) => {
+      e.preventDefault
+      setSubmitting(true)
+  
+      try {
+        const responce = await fetch(`/api/prompt/${paramsId}`, {
+          method: "PATCH",
+          body:JSON.stringify({
+            prompt: post.prompt,
+            tag: post.tag
+          })
+        });
+  
+        if(responce.ok){
+          router.push("/");
+        }
+      } catch (error) {
+        console.log(error);
+      }finally{
+        setSubmitting(false);
+      }
+    }
+    return (
+      <Suspense>
+         <From 
+          type="Update "
+          post={post}
+          setPost={setPost}
+          submitting={submitting}
+          handleSubmit={updatePrompt}
+          >
+        </From>
+      </Suspense>
+    )
+}
+
+export default EditPrompt;
